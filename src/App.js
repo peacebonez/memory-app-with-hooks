c375a683be46a1010a9baeb67e009727903e8361
peacebonez <kmpariso12@gmail.com>
2020-05-25 18:16:35 -0400

    fixed Game Over display
    text-align center

--- src/App.js
+++ src/App.js
@@ -15,22 +15,27 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [gameOver, setGameOver] = useState(false);
 
+  //sets cards as a shuffled array of cards
   useEffect(() => {
     setCards(initializeDeck());
   }, []);
 
+  //preloads the images to the game before the cards are flipped
   useEffect(() => {
     preloadImages();
   }, [cards]);
 
+  //handles game over state
   useEffect(() => {
     if (solved.length === 16) handleGameOver();
   }, [solved]);
 
+  //sets game state to playing
   const startGame = () => {
     setIsPlaying(true);
   };
 
+  //increments game timer
   const timer = useEffect(() => {
     if (isPlaying) {
       let timer = setInterval(() => {
@@ -41,6 +46,7 @@ function App() {
     }
   }, [time, isPlaying]);
 
+  //resets state of game to init
   const resetGame = () => {
     resetCards();
     setSolved([]);
@@ -51,26 +57,28 @@ function App() {
     setCards(initializeDeck());
   };
 
+  //handles game over.... derrrrr
   const handleGameOver = () => {
     setGameOver(true);
     setIsPlaying(false);
   };
 
+  //executes when a card is clicked
   const handleClick = (id) => {
-    if (!isPlaying) setIsPlaying(true);
+    if (!isPlaying) setIsPlaying(true); //if game not started, start game on first click
     setDisabled(true);
     if (flipped.length === 0) {
       setFlipped([id]);
       setDisabled(false);
     } else {
-      if (sameCardClicked(id)) return;
-      setFlipped([...flipped, id]);
-      setMoves((move) => move + 1);
+      if (sameCardClicked(id)) return; //prevents clicking on same card twice
+      setFlipped([...flipped, id]); //array of flipped cards max length is 2
+      setMoves((move) => move + 1); //increments a move per pair of cards flipped
       if (isMatch(id)) {
-        setSolved([...solved, flipped[0], id]);
+        setSolved([...solved, flipped[0], id]); //if match, add card Id's to solved arr
         resetCards();
       } else {
-        setTimeout(resetCards, 1500);
+        setTimeout(resetCards, 1500); // timeout flips card back over if no match
       }
     }
   };
@@ -80,15 +88,22 @@ function App() {
     setDisabled(false);
   };
 
-  //Is user clicking same card twice?
+  //Is user clicking same card twice boolean
   const sameCardClicked = (id) => flipped.includes(id);
 
+  //determines if card pair is a match
   const isMatch = (id) => {
+    //matches just clicked card to in cards array
     const justClicked = cards.find((card) => card.id === id);
+
+    //flipped[0] is the 1st of the 2 cards in the pair to be flipped
     const alreadyFlipped = cards.find((card) => flipped[0] === card.id);
+
+    //determines if a match is made
     return alreadyFlipped.type === justClicked.type;
   };
 
+  //preloads the images to the game before the cards are flipped
   const preloadImages = () => {
     cards.map((card) => {
       const src = card.type.front;
@@ -96,7 +111,6 @@ function App() {
     });
   };
 
-  console.log("solved:", solved);
   return (
     <div className="App">
       <Heading time={time} moves={moves} resetGame={resetGame} />
